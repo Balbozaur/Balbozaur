8f6b74ae69fbcc5fe89a43742711e653ed950c41
Balbozaur <92030302+Balbozaur@users.noreply.github.com>
2022-04-12 14:53:29 +0200

    Add files via upload

---
+++ Dragon.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+#include <time.h>
+using namespace std;
+class Elements {
+    vector<string> Tablica; 
+};
+
+class PlayersAbillities {
+public:
+    void feed()
+    {
+
+    }
+    void play()
+    {
+
+    }
+    void hunt()
+    {
+
+    }
+
+};
+
+class Dragon {
+
+public:
+    string Title;
+    string Name;
+    int ArmorClass;
+    int HitPoints;
+    int Hunger;
+    int Love;
+    void Birth()
+    {
+        srand(time(NULL));
+        cin >> Name;
+        vector<string> Title;
+        int NewTitle = rand() % Title.size - 1;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
